2cc710dcbff55e33d9d54540e71b2286369383bf
Rohit B. Kharmate <121004090+KharmateRB@users.noreply.github.com>
2023-12-27 10:28:37 +0530

    Add files via upload
    login page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Login from './Login';
+import './App.css'; // Import the CSS file
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
